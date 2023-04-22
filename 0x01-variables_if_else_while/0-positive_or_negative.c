21c45a6c3a1723a6de8652ee77af775e8dcf23a8
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-04-21 18:48:44 -0700

    Positive/Negative.

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/*
-main - Entry point 
-
-Return: 0 (success)
+/**
+ * main - prints the sign of the number
+ *
+ * Return: 0
 */
 int main(void)
 {
