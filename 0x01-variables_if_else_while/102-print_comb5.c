ec917719661eaf92e3079eb19d35c38c4bf5d94f
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-04-24 00:40:55 +0100

    combo3

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 99; i++)
+	{
+		for (j = 1 ; j < 100; j++)
+		{
+			putchar(i / 10 + 48);
+			putchar(i % 10 + 48);
+			putchar(' ');
+			putchar(j / 10 + 48);
+			putchar(j % 10 + 48);
+			if (i != 98 || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
