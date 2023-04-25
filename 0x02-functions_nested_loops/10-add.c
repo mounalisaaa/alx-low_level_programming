d6f4ce1ee322c50a90bddfe1f96a4e817bada230
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-04-25 19:05:00 +0100

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two ints
+ * @a : number1 to calculate
+ * @b : number2
+ * Return: Always 0.
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * times_table - the 9 times table
+ *
+ * Return: 0
+ */
+void times_table(void)
+{
+	int i, j;
+	int k;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			k=i*j;
+		}
+	}
+}
