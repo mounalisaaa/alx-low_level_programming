469666ada034ff3ef99b2d0ec98a9106763f7a1c
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-04-25 22:42:49 +0100

    finally

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,28 @@ void times_table(void)
 	{
 		for (j = 0; j < 10; j++)
 		{
-			k=i*j;
+			k = i * j;
+			if (k > 9)
+			{
+				_putchar(k / 10 + '0');
+				_putchar(k % 10 + '0');
+			}
+			else if (j != 0)
+			{
+				_putchar(' ');
+				_putchar(k + '0');
+			}
+			else
+			{
+				_putchar(k + '0');
+			}
+			if (j != 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
 		}
+		_putchar('$');
+		_putchar('\n');
 	}
 }
