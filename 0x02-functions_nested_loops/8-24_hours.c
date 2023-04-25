d1d94954e2ca95cc62ccad49234b39a7b7ab9373
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-04-25 17:00:22 +0100

    Every min counts

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+void jack_bauer(void)
+{
+	int i, j;
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+	
+}
