5e53716d1a99c825cd6599129e00d1fe72eb7ff1
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-04-25 15:40:44 +0100

    fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,9 +7,14 @@
  */
 int main(void)
 {
-	char function[] = "_putchar";
-
-	_putchar("%s\n", function);
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
