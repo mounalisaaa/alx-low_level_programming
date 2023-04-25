4e3586d89d0337e55db2154dc00e249dbc0b4b88
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-04-26 00:39:41 +0100

    Fibonacci3.1

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,23 +1,40 @@
 #include <stdio.h>
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - prints 98 first fibos
+ * Return: ALways 0 (Success)
  */
 int main(void)
 {
-	int n = 98;
-	unsigned long long a = 1, b = 2;
+	unsigned long int a, b, c, n, m, x, y;
 
-	printf("%llu, %llu", a, b);
-	for (int i = 2; i < n; i++)
+	b = 1;
+	c = 2;
+
+	printf("%lu", b);
+
+	for (a = 1; a < 91; a++)
 	{
-		unsigned long long c = a + b;
-
-		printf(", %llu", c);
-		a = b;
-		b = c;
+		printf(", %lu", c);
+		c = c + b;
+		b = c - b;
 	}
+
+	n = b / 1000000000;
+	m = b % 1000000000;
+	x = c / 1000000000;
+	y = c % 1000000000;
+
+	for (a = 92; a < 99; ++a)
+	{
+		printf(", %lu", x + (y / 1000000000));
+		printf("%lu", y % 1000000000);
+		x = x + n;
+		n = x - n;
+		y = y + m;
+		m = y - m;
+	}
+
 	printf("\n");
+
 	return (0);
 }
