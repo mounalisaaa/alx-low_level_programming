d5639ad6dbf33dbb203d2db08ede824bf91b5912
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-04-25 11:46:43 +0100

    fixed

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 #include "main.h"
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
-int main(void){
+int main(void)
+{
 	char function[] = "_putchar";
+
 	printf("%s\n", function);
+
 	return (0);
 }
