1cb69ace7b9b7ec72e3491263d83a4a0f6e12f59
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-04-27 14:25:10 +0100

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 #include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int i;
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0)
