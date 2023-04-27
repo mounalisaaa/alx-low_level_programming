e98423f6168383056784c145e271987a05f9bb33
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-04-27 15:05:51 +0100

    Ughh

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,21 +13,23 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
-			printf("FizzBuzz ");
+			printf("FizzBuzz");
 		}
 		else if (i % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 
 		else if (i % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
