48269b69c2bf68df0cf376c8396e1e7db6623744
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-04-27 07:00:59 +0100

    from 0 to 9

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,4 +1,8 @@
 #include "main.h"
+/**
+  * print_numbers - function that prints the numbers, from 0 to 9
+  * Return: void
+  */
 void print_numbers(void)
 {
 	int i;
@@ -6,4 +10,5 @@ void print_numbers(void)
 	{
 		_putchar(i + '0');
 	}
+	_putchar('\n');
 }
