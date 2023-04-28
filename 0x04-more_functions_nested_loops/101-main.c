923ab3ddf85e1d1a7c6cd85df82b24016cab3920
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-04-28 03:33:14 +0100

    test

--- 0x04-more_functions_nested_loops/101-main.c
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -7,7 +7,7 @@
  */
 int main(void)
 {
-    print_number(98);
+    print_number(-2147483648);
     _putchar('\n');
     print_number(402);
     _putchar('\n');
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,6 +7,12 @@
 
 void print_number(int n)
 {
+	if (n == -2147483648)
+	{
+		_putchar('-');
+		_putchar('2');
+		print_number(147483648);
+	}
 	if (n < 0)
 	{
 		_putchar('-');
