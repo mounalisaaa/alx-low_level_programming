1533f5bb15428904d32955a52981ce9e1a5672fc
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-04-28 03:06:07 +0100

    Done

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_number - Print integer.
+ * @n: integer
+ * Return: Always 0 (Success)
+ */
+
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	if (n >= 10)
+	{
+		print_number(n / 10);
+	}
+	_putchar(n % 10 + '0');
+}
