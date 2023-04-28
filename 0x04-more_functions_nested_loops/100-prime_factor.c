06b2bdcbd4a742a4a71f550821b5e54061ce7a1c
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-04-28 02:33:50 +0100

    test

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	long int num = 612852475143;
+	long int largest_prime_factor = 1;
+	long int factor;
+
+	while (num % 2 == 0)
+	{
+		largest_prime_factor = 2;
+		num /= 2;
+	}
+	for (factor = 3; factor * factor <= num; factor += 2)
+	{
+		while (num % factor == 0)
+		{
+			largest_prime_factor = factor;
+			num /= factor;
+		}
+	}
+	if (num > 2)
+	{
+		largest_prime_factor = num;
+	}
+	printf("%ld\n", largest_prime_factor);
+	return (0);
+}
