84ccded4089b65b7f2c3e65986e401eb7d3377a3
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-04-28 03:50:42 +0100

    fixed

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -7,20 +7,24 @@
 
 void print_number(int n)
 {
-	if (n == -2147483648)
-	{
-		_putchar('-');
-		_putchar('2');
-		print_number(147483648);
-	}
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		if (n == -2147483648)
+		{
+			_putchar('2');
+			n %= 1000000000;
+		}
+		print_number(-n);
 	}
-	if (n >= 10)
+	else if (n <= 9)
+	{
+		_putchar(n + '0');
+	}
+
+	else
 	{
 		print_number(n / 10);
+		print_number(n % 10);
 	}
-	_putchar(n % 10 + '0');
 }
