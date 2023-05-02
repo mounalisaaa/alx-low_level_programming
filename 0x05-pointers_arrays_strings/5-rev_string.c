0c48aa090471f8c7be50f22dc9bb2262ad10c8b5
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-02 07:02:46 +0100

    Task5.1

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,9 +1,27 @@
 #include "main.h"
 /**
+ *  _strlen -  calculates the length of a string.
+ * @s: pointer to a string.
+ * Return: lenght.
+ */
+
+int _strlen(char *s)
+{
+	int lenght = 0;
+
+	while (*s != '\0')
+	{
+		lenght++;
+		s++;
+	}
+	return (lenght);
+}
+/**
  *  rev_string - it reverse a string.
  * @s: string parameter.
  * Return: Always 0.
  */
+
 void rev_string(char *s)
 {
 	int i = 0;
