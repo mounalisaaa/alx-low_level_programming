0063154b778dad71690a4972ddd40ddeb46ba24c
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-02 08:38:31 -0700

    ana n9ala

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,14 +1,29 @@
 #include "main.h"
+/**
+  * puts_half - prints half of a string
+  * @str: string
+  *
+  * Return: void
+  */
 
 void puts_half(char *str)
 {
 	int len = 0;
-	int n;
-	while (str[len] != '\0')
+	int i;
+
+	while (*(str + len) != '\0')
 		len++;
 
 	if (len % 2 == 0)
-		n = len / 2;
+		i = len / 2;
 	else
-		i = (len + 1) / 2; // to be continued
+		i = (len + 1) / 2;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar('\n');
 }
+
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * print_array - prints n elements of an array
+  * @a: the array
+  * @n: the number of element to print
+  *
+  * Return: void
+  */
+
+void print_array(int *a, int n)
+{
+	int i = 0;
+
+	while (i < n)
+	{
+		printf("%d", *(a + i));
+
+		if (i != (n - 1))
+			printf(", ");
+		i++;
+	}
+	printf("\n");
+}
