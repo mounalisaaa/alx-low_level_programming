ebe86d813a87612cd06d919a0380b1678f0f5dce
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-02 08:45:09 +0100

    just to save

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void puts_half(char *str)
+{
+	int len = 0;
+	int n;
+	while (str[len] != '\0')
+		len++;
+
+	if (len % 2 == 0)
+		n = len / 2;
+	else
+		i = (len + 1) / 2; // to be continued
+}
