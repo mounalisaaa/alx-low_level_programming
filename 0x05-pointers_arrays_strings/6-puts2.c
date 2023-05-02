a994ff17bd3866e94450c979d8bbe89c2c04b33a
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-02 06:44:41 +0100

    Task6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - print every other character of str starting with the first character
+ * @str: pointer to string.
+ * Return: Always 0.
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
