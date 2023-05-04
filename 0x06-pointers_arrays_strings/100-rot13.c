594ccf24dc6c12dcad7569f70c525ca8244245d2
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-04 22:24:51 +0100

    rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * rot13 - function that encodes a string using rot13.
+ *@str: string.
+ * Return: str.
+ */
+char *rot13(char *str)
+{
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if ((str[i] >= 'a' && str[i] <= 'm' || str[i] >= 'A' && str[i] <= 'M'))
+		{
+			str[i] += 13;
+		}
+		else if ((str[i] >= 'n' && str[i] <= 'z' || str[i] >= 'N' && str[i] <= 'Z'))
+		{
+			str[i] -= 13;
+		}
+	}
+	return (str);
+}
