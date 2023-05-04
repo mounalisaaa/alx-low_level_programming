9e621d8d2733a10ad0379e7caaab3784b40de43e
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-04 22:56:20 +0100

    task9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_number - Print integer.
+ * @n: integer
+ * Return: Always 0 (Success)
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		if (n == -2147483648)
+		{
+			_putchar('2');
+			n %= 1000000000;
+		}
+		print_number(-n);
+	}
+	else if (n <= 9)
+	{
+		_putchar(n + '0');
+	}
+
+	else
+	{
+		print_number(n / 10);
+		print_number(n % 10);
+	}
+}
