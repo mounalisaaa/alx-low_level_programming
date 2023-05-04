3f9646315d300793587e46919e9241f6d0fe8a60
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-04 22:35:29 +0100

    rot13 fixed

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -6,15 +6,15 @@
  */
 char *rot13(char *str)
 {
-	int i, j;
+	int i;
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if ((str[i] >= 'a' && str[i] <= 'm' || str[i] >= 'A' && str[i] <= 'M'))
+		if ((str[i] >= 'a' && str[i] <= 'm') || (str[i] >= 'A' && str[i] <= 'M'))
 		{
 			str[i] += 13;
 		}
-		else if ((str[i] >= 'n' && str[i] <= 'z' || str[i] >= 'N' && str[i] <= 'Z'))
+		else if ((str[i] >= 'n' && str[i] <= 'z') || (str[i] >= 'N' && str[i] <= 'Z'))
 		{
 			str[i] -= 13;
 		}
