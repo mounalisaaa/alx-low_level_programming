fa525b3c0edd824cc4d14909c35c6a06b510afb9
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-04 20:20:31 +0100

    a to A

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper -  function that changes all lowercases to uppercases.
+ *@str: pointer.
+ * Return: str.
+ */
+char *string_toupper(char *str)
+{
+	char *ptr = str;
+
+	while (*ptr != '\0')
+	{
+		if (*ptr >= 'a' && *ptr <= 'z')
+		{
+			*ptr -=  ('a' - 'A');
+		}
+		ptr++;
+	}
+	return (str);
+}
