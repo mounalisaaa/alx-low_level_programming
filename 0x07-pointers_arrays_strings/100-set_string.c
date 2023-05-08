1df8fa1d093ca6a89245679521588eba7034485c
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-08 14:00:16 +0100

    double misery

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - set the value of a ponter to a char
+ * @s: double pointer
+ * @to: pointer to set
+ * Return: a char
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
