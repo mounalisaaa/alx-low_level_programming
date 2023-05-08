e4732d495fb2b1d0178d20f8a77e81b05c20f3ad
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-08 11:41:44 +0100

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strchr - function that locates a character in a string.
+ * @s: pointer.
+ * @c: character
+ * Return: s or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return ('\0');
+}
