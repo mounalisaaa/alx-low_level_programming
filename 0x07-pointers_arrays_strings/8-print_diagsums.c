c63c5f598bfea790eeb2a3798dfe5518752066c1
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-08 13:55:05 +0100

    matrix fixed

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,7 +12,7 @@ void print_diagsums(int *a, int size)
 	int sum1 = 0;
 	int sum2 = 0;
 
-	for (i = 0, i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		sum1 += a[(size + 1) * i];
 		sum2 += a[(size - 1) * (i + 1)];
