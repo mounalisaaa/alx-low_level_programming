285877ef92f6f2fd27ed1a4f21f9fc26bcc6c7c7
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-12 17:22:19 +0100

    task3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -5,20 +5,17 @@
  * @argc: int.
  * @argv: arr to str.
  * Return: 1 or 0.
-*/
+ */
 int main(int argc, char *argv[])
 {
 	int result;
 
-	result = atoi(argv[1]) * atoi(argv[2]);
-	if (argc == 3)
-	{
-		printf("%d\n", result);
-	}
-	else
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	result = atoi(argv[1]) * atoi(argv[2]);
+	printf("%d\n", result);
 	return (0);
 }
