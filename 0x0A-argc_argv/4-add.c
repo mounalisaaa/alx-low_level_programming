82a05759c56c2e09a29d6ab181df92af17fbcc6c
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-12 19:07:25 +0100

    push

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,8 +1,31 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
-int main (int argc, char **argv)
+int main(int argc, char **argv)
 {
-	
+	int num;
+	int sum = 0;
+	int i, j;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[i]);
+		sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
 }
-int 
