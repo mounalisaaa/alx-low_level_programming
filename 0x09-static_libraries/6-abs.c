90225906d61f8efa120a98061206cb9dcb42ed8d
mounalisaaa <115364931+mounalisaaa@users.noreply.github.com>
2023-05-12 19:55:11 +0100

    Add files via upload

--- 0x09-static_libraries/6-abs.c
+++ 0x09-static_libraries/6-abs.c
@@ -1,18 +1,3 @@
 #include "main.h"
-/**
- * _abs - function that computes the absolute value of an integer.
- *  @n: int type number
- * Return: 1 or 0
- */
-
 int _abs(int n)
-{
-	if (n < 0)
-	{
-		return (-n);
-	}
-	else
-	{
-		return (n);
-	}
-}
+{}
--- 0x09-static_libraries/9-strcpy.c
+++ 0x09-static_libraries/9-strcpy.c
@@ -1,23 +1,3 @@
 #include "main.h"
-
-/**
-  * _strcpy - copys str to another
-  * @dest: destination string
-  * @src: the str that we gonna copy
-  *
-  * Return: the pointer to dst
-  */
-
 char *_strcpy(char *dest, char *src)
-{
-	int i = 0;
-
-	while (*(src + i))
-	{
-		*(dest + i) = *(src + i);
-		i++;
-	}
-	*(dest + i) = '\0';
-
-	return (dest);
-}
+{}
