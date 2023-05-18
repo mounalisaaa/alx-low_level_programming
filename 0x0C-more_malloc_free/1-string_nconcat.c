f9dfd33056ac2f1d7dd831898b57cb2697588aaf
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-18 13:29:51 +0100

    test

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,7 +19,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	len = _strlen(s1) + _strlen(s2) + 1;
+	len = _strlen(s1) + n + 1;
 
 	str = malloc(len);
 	if (str == NULL)
