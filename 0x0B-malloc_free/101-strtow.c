2bed050dc458ea0b8520ec37987867e863d29cc6
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-18 11:58:34 +0100

    to be continued

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,18 @@
+#include"main.h"
+char **strtow(char *str)
+{
+	int len;
+	int num_words;
+	int i;
+
+	if (str == NULL || str == "")
+		return(NULL);
+
+	len = _strlen(str);
+
+	for(i=0; i<len; i++)
+	{
+		if (_strchr()) // to be continued
+	}
+
+}
