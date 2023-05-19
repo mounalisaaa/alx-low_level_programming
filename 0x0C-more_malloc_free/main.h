50de28a4c4447372c074f968493f56a4334c0c0c
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-19 13:37:31 +0100

    multiplication

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,3 +6,4 @@ int _strlen(char *s);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+int _putchar(char c);
