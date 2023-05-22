bcaa7e51354711ff89cfc716ca5932d0e7f17eac
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-22 20:21:19 +0100

    freee

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,11 +1,14 @@
-#include<stdlib.h>
+#include <stdlib.h>
 #include "dog.h"
 /**
  * free_dog - function that frees dogs.
  * @d: ptrn to struct.
-*/
+ */
 void free_dog(dog_t *d)
 {
+	if (d == NULL)
+		return;
+
 	free(d->name);
 	free(d->owner);
 	free(d);
