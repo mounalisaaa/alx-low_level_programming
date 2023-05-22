336ee1291798999cb5d506e93c933eed7ad27d77
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-22 20:03:33 +0100

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -25,8 +25,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	new_dog->owner = _strdup(owner);
 	if (new_dog->owner == NULL)
 	{
-		free(new_dog);
 		free(new_dog->name);
+		free(new_dog);
 		return (NULL);
 	}
 
