e4daea2cb7c4ef6d1fc623387815b6f8e4b81381
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-22 16:40:34 +0100

    dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,13 @@
 #ifndef MYFILE_H
 #define MYFILE_H
-
-struct dog {
+/**
+ * struct dog - has 3 elements.
+ * @name: 1st element.
+ * @age: 2nd element.
+ * @owner: 3rd element.
+*/
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
