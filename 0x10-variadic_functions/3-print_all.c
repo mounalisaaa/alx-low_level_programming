75c5b5b404912cc6feecb2062b6f4ea07e2e7194
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-25 17:57:14 +0100

    print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,42 +9,40 @@ void print_all(const char *const format, ...)
 {
 	va_list args;
 	int bi = 0;
-	char *s, *sep = "";
-
+	char *s;
 	va_start(args, format);
 
-	if (format)
+	
+	while (format && format[bi])
 	{
-		while (format[bi])
+		switch (format[bi])
 		{
-			switch (format[bi])
-			{
-			case 'c':
-				printf("%s%c", sep, va_arg(args, int));
-				break;
-			case 'i':
-				printf("%s%d", sep, va_arg(args, int));
-				break;
-			case 'f':
-				printf("%s%f", sep, va_arg(args, double));
-				break;
-			case 's':
-				s = (va_arg(args, char *));
-				if (s == NULL)
-					s = "(nil)";
+		case 'c':
+			printf("%c", va_arg(args, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(args, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(args, double));
+			break;
+		case 's':
+			s = (va_arg(args, char *));
+			if (s == NULL)
+				s = "(nil)";
 
-				printf("%s%s", sep, s);
-				break;
-			default:
-				bi++;
-				continue;
-			}
-			sep = ", ";
+			printf("%s", s);
+			break;
+		default:
 			bi++;
+			continue;
 		}
-		printf("\n");
-		va_end(args);
+		if (bi != _strlen(format) - 1)
+			printf(", ");
+		bi++;
 	}
+	printf("\n");
+	va_end(args);
 }
 /**
  *  _strlen -  calculates the length of a string.
