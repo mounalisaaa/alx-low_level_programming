ea5a211e258f71611750a2784e7c62e6721f60e2
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-05-25 12:55:29 +0100

    sum

---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,3 @@
+Why did the programmer quit using variadic functions?
+
+Because they couldn't handle the arguments!
---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VA_H
+#define VA_H
+
+#include<stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+
+
+#endif
