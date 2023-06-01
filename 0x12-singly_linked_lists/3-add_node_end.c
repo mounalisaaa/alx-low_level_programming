3c1a813179346715fb48b73d0d848acd9f46310e
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-06-01 17:14:01 +0100

    end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,48 +1,31 @@
 #include "lists.h"
+
 /**
- * add_node_end - add new node as the last node.
- * @head: ptr to ptr to 1st node.
- * @str: str.
- * Return: add of new element.
+ * add_node_end - adds a new node at the end of list_t list
+ * @head: double pointer to list_t
+ * @str: pointer to the first node in list_t
+ * Return: a pointer to the new node or NULL if it failed
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
-	list_t *current;
+	list_t *new = malloc(sizeof(list_t));
+	list_t *curr = *head;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
+	if (!new)
 		return (NULL);
 
 	new->str = strdup(str);
-	new->len = _strlen(str);
+	new->len = strlen(str);
 	new->next = NULL;
 
-	if (*head == NULL)
-	{
+	if (!*head)
 		*head = new;
-	}
-	current = *head;
-	while (current->next != NULL)
+	else
 	{
-		current = current->next;
+		while (curr->next)
+			curr = curr->next;
+		curr->next = new;
 	}
-	current->next = new;
-
 	return (new);
-}
-/**
- * _strlen - func.
- * @str: str.
- * Return: len.
- */
-int _strlen(const char *str)
-{
-	int i = 0;
-
-	while (str[i])
-	{
-		i++;
-	}
-	return (i);
 }
