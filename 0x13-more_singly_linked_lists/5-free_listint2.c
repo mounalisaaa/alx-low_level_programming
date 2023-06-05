a64bc10d7358f8da436124cef99d19398300d60f
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-06-05 16:14:23 +0100

    free fixed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,13 +2,13 @@
 /**
  * free_listint2 -  frees a listint_t list.
  * @head: pointer to pointer to 1st node.
-*/
+ */
 void free_listint2(listint_t **head)
 {
 	listint_t *curr = *head;
 	listint_t *ptr;
 
-	if (!*head)
+	if (!head)
 		return;
 
 	while (curr)
