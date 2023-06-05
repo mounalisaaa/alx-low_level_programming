2b2766619b7c8cfcd8e4db9420d586591db3d186
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-06-05 21:27:46 +0100

    meh

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -30,8 +30,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		curr = curr->next;
 	}
-	
 	temp->next = curr->next;
 	curr->next = temp;
+	if (curr->next == NULL)
+	{
+		temp->next = NULL;
+		curr->next = temp;
+		return (temp);
+	}
 	return (temp);
 }
