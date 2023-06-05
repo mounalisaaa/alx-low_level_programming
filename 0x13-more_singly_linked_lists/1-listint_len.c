dbf55c69a1fa9106c469e95b27af0d2670879c92
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-06-05 10:28:18 +0100

    node at the beginning

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 /**
  * listint_len - returns the number of elements in a linked listint_t list.
- * @h: pointer the the first node in the list.
+ * @h: pointer to the first node in the list.
  * Return: numbers of nodes.
  */
 size_t listint_len(const listint_t *h)
