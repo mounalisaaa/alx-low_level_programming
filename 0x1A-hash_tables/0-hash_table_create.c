dae62c5dc21eb1af3b0a3310303a4380e16242f4
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-10-04 19:07:26 +0100

    create ht

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t* map = malloc(sizeof(hash_table_t));
+	if (!map)
+		return (NULL);
+	map->size = size;
+	map->array = malloc(sizeof(hash_node_t *) * size);
+	if (!map->array)
+		return (NULL);
+	return (map);
+}
