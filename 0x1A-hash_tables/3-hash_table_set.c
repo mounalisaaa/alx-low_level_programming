be1bd2cd3a09077c341897340cf07b00e073dd9f
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-10-04 21:00:31 +0100

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,13 @@
 #include "hash_tables.h"
 
+/**
+ * _strdup - Duplicates a string in a newly allocated memory space.
+ *
+ * @s: The string to be duplicated.
+ *
+ * Return: A pointer to the newly allocated duplicated string, or NULL
+ *         if memory allocation fails or if @s is NULL.
+ */
 char *_strdup(const char *s)
 {
 	int i;
@@ -21,6 +29,14 @@ char *_strdup(const char *s)
 	return (new);
 }
 
+/**
+ * _strcmp - Compares two strings character by character.
+ *
+ * @s1: The first string to compare.
+ * @s2: The second string to compare.
+ *
+ * Return: int
+ */
 int _strcmp(char *s1, char *s2)
 {
 	int i;
@@ -31,12 +47,22 @@ int _strcmp(char *s1, char *s2)
 	return (s1[i] - s2[i]);
 }
 
+/**
+ * hash_table_set - Adds or updates a key-value pair in a hash table.
+ *
+ * @ht: A pointer to the hash table.
+ * @key: The key to be added or updated.
+ * @value: The value associated with the key.
+ *
+ * Return: 1 if the operation succeeds, 0 otherwise.
+ */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int key_idx;
 	hash_node_t *temp;
 	hash_node_t *item;
-	
+
 	if (!key || !ht || !value)
 		return (0);
 	key_idx = key_index((const unsigned char *)key, ht->size);
