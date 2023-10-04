778ba4d41b846a33ac7e02e8a0687474a779ff4d
mounalisaaa <mounaelyaacoubi15@gmail.com>
2023-10-04 20:31:47 +0100

    3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -27,6 +27,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *temp;
 	hash_node_t *item;
 	
+	if (!key || !ht || !value)
+		return (0);
 	key_idx = key_index((const unsigned char *)key, ht->size);
 	item = malloc(sizeof(hash_node_t));
 	if (!item)
